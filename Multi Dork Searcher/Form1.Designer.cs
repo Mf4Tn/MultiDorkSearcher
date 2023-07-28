27a7a7401a625f181376d689c1f4884bf9a3d720
Mf4Tn <75338199+Mf4Tn@users.noreply.github.com>
2023-07-28 07:49:52 +0100

    Add files via upload

---
+++ Multi Dork Searcher/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Multi_Dork_Searcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.DetectUrls = false;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 130);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(275, 448);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(90, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(114, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(-11, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Dork";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(210, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 78);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(90, 41);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(114, 20);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(-11, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Pages";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Google",
+            "Bing"});
+            this.comboBox1.Location = new System.Drawing.Point(90, 67);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(114, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.Text = "Google";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(-14, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 23);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Search Engine";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Location = new System.Drawing.Point(100, 94);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(104, 24);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Full Link";
+            this.checkBox1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(313, 590);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mutli Dork Searcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Multi Dork Searcher/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Leaf.xNet;
+using System.Text.RegularExpressions;
+using System.Threading;
+using HtmlAgilityPack;
+
+namespace Multi_Dork_Searcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void google_search(string dork,string pages)
+        {
+            new Thread(() =>
+            {
+                for(int i = 0; i < Int32.Parse(pages); i++)
+                {
+                    try
+                    {
+                        HttpRequest request = new HttpRequest();
+                        var dro = Uri.EscapeDataString(dork);
+                        request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36";
+                        var response = request.Get($"https://www.google.com/search?q={dro}&start={i}0");
+
+                        string pattern = @"https?://(www\.)?[^""\s<>']+";
+                        MatchCollection matches = Regex.Matches(response.ToString(), pattern);
+                        List<string> links = new List<string>();
+
+                        var htmlDocument = new HtmlAgilityPack.HtmlDocument();
+                        htmlDocument.LoadHtml(response.ToString());
+
+                        var resultNodes = htmlDocument.DocumentNode.SelectNodes("//div[@class='yuRUbf']//a[@href]");
+                        if (resultNodes != null)
+                        {
+                            foreach (var node in resultNodes)
+                            {
+                                string link = node.GetAttributeValue("href", "");
+                                string title = Regex.Replace(node.InnerText, "<.*?>", string.Empty); // Remove HTML tags
+                                var temp = link.Replace("://", "");
+                                var domain = link.Split('/')[2];
+                                domain = link.Split(':')[0] + "://" + domain;
+                                //MessageBox.Show(link);
+                                if (!links.Contains(domain) && !domain.Contains("google.") && !domain.Contains("googleusercontent"))
+                                {
+
+                                    links.Add(domain);
+                                    richTextBox1.Invoke(new MethodInvoker(delegate
+                                    {
+                                        checkBox1.Invoke(new MethodInvoker(delegate
+                                        {
+                                            if (checkBox1.Checked)
+                                            {
+                                                richTextBox1.Text += link + "\n";
+                                            }
+                                            else
+                                            {
+                                                richTextBox1.Text += domain + "\n";
+                                            }
+                                        }));
+
+                                        // MessageBox.Show(link);
+                                    }));
+                                }
+                            }
+                        }
+                        
+
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString());
+                    }
+                }
+                MessageBox.Show("Finished!","Notice",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            })
+            { IsBackground = true }.Start();
+        }
+
+        public void bing_search(string dork,string pages)
+        {
+            new Thread(() =>
+            {
+                for (int i = 0; i < Int32.Parse(pages); i++)
+                {
+                    try
+                    {
+                        HttpRequest request = new HttpRequest();
+                        var dro = Uri.EscapeDataString(dork);
+                        request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36";
+                        var response = request.Get($"https://www.bing.com/search?q={dro}&first={pages}0");
+
+                        string pattern = @"https?://(www\.)?[^""\s<>']+";
+                        MatchCollection matches = Regex.Matches(response.ToString(), pattern);
+                        List<string> links = new List<string>();
+                        var htmlDocument = new HtmlAgilityPack.HtmlDocument();
+                        htmlDocument.LoadHtml(response.ToString());
+
+                        var resultNodes = htmlDocument.DocumentNode.SelectNodes("//li[@class='b_algo']//h2//a[@href]");
+                        if (resultNodes != null)
+                        {
+                            foreach (var node in resultNodes)
+                            {
+                                string link = node.GetAttributeValue("href", "");
+                                string title = node.InnerText;
+                                //MessageBox.Show(link);
+                                var temp = link.Replace("://", "");
+                                var domain = link.Split('/')[2];
+                                domain = link.Split(':')[0] + "://" + domain;
+                                if (!links.Contains(domain) && !domain.Contains("bing.") && !domain.Contains("bing"))
+                                {
+
+                                    links.Add(domain);
+                                    richTextBox1.Invoke(new MethodInvoker(delegate
+                                    {
+                                        checkBox1.Invoke(new MethodInvoker(delegate
+                                        {
+                                            if (checkBox1.Checked)
+                                            {
+                                                richTextBox1.Text += link + "\n";
+                                            }
+                                            else
+                                            {
+                                                richTextBox1.Text += domain + "\n";
+                                            }
+                                        }));
+
+                                        // MessageBox.Show(link);
+                                    }));
+                                }
+                            }
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString());
+                    }
+                }
+                MessageBox.Show("Finished!", "Notice", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            })
+            { IsBackground = true }.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Google")
+            {
+                google_search(textBox1.Text, numericUpDown1.Value.ToString());
+            }else if(comboBox1.Text == "Bing")
+            {
+                bing_search(textBox1.Text, numericUpDown1.Value.ToString());
+            }
+        }
+    }
+}
